5359b428653d91cb44cd7a512d95e2f3d193fdf6
Júlia Facó <juliafaco2207@gmail.com>
2025-06-21 16:50:11 -0300

    added: "Goodbye" message

--- src/main/java/julia/faco/Main.java
+++ src/main/java/julia/faco/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, julia");
         System.out.println("Hello, sergio");
+        System.out.println("Bye bye bye");
 
     }
 }
